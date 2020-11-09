352b71d0fd771ca1a6b686e7415da45d491418d8
wizard1238 <49770335+wizard1238@users.noreply.github.com>
2020-11-09 11:14:45 -0800

    added json to mongodb feature.

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
+const { json } = require('express');
 var express = require('express');
 var router = express.Router();
 
@@ -5,5 +6,36 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+const mongoose = require("mongoose")
+router.post('/upload', function(req, res) {
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send('No files were uploaded.');
+  }
+
+  let modelName = req.body.mongooseModel
+
+  mongoose.connect(req.body.mongooseURL, {useNewUrlParser: true, useUnifiedTopology: true})
+
+  var dataSchema = new mongoose.Schema({}, {strict: false})
+  var dataModel = mongoose.model(modelName, dataSchema)
+
+  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+  let File = req.files.File.data.toString()
+  let commaFile = File.replace(/\n/g, "~")
+  let Array = commaFile.split('~')
+  for (i in Array) {
+    Array[i] = JSON.parse(Array[i])
+  }
+  dataModel.collection.insertMany(Array, function(err, r) {
+    if (err) console.log(err)
+  })
+
+  res.send('File uploaded!');
+});
+
+router.get('/uploadpage', function(req, res, next) {
+  res.render('upload')
+})
 
 module.exports = router;
