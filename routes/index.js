e66de489d18e4e788efa020d161c5796a511c93c
wizard1238 <49770335+wizard1238@users.noreply.github.com>
2020-11-12 22:16:10 -0800

    added uploading svgs from folder

--- routes/index.js
+++ routes/index.js
@@ -24,7 +24,7 @@ router.post('/upload', function(req, res) {
   let File = req.files.File.data.toString()
   let commaFile = File.replace(/\n/g, "~")
   let Array = commaFile.split('~')
-  for (i in Array) {
+  for (let i in Array) {
     Array[i] = JSON.parse(Array[i])
   }
   dataModel.collection.insertMany(Array, function(err, r) {
@@ -36,6 +36,50 @@ router.post('/upload', function(req, res) {
 
 router.get('/uploadpage', function(req, res, next) {
   res.render('upload')
+})
+
+router.post('/upload/folder/svg', function(req, res) {
+  
+  let modelName = req.body.mongooseModel
+
+  mongoose.connect(req.body.mongooseURL, {useNewUrlParser: true, useUnifiedTopology: true})
+
+  var characterSchema = new mongoose.Schema({
+    character: String,
+    definition: String,
+    pinyin: Array,
+    decomposition: String,
+    radical: String,
+    matches: Array
+  }, {collection: "characters"})
+  let characterModel = mongoose.model('characters', characterSchema)
+  
+  var svgSchema = new mongoose.Schema({
+    character: String,
+    svg: String,
+  })
+  let svgModel = mongoose.model(modelName, svgSchema)
+
+  characterModel.find({}, function(err, chars) {
+    for (let i in req.files.File) {
+      let char = chars[i].character
+      let svg = req.files.File[i].data.toString()
+      
+      let newSvg = new svgModel({
+        character: char,
+        svg: svg,
+      })
+      newSvg.save()
+    }
+    console.log(chars.length)
+    console.log(req.files.File.length)
+  })
+
+  res.sendStatus(200)
+})
+
+router.get('/upload/folder/svg', function(req, res, next) {
+  res.render('svgFolder')
 })
 
 module.exports = router;
